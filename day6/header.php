c32900f90dbc59d56078a9b994f8250378e96963
unknown <sunreverse.0311.blue@gmail.com>
2020-03-29 22:20:32 +0900

    first wordpress

---
+++ day6/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php wp_head(); ?>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    </head>
+    <body>
+        <header>
+        </header>
---
+++ day6/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+<div>
+    Coming Soon...!
+</div>
+<?php get_footer(); ?>
